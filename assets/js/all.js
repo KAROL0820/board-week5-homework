b77cc46e8227fd943abf270dd7983724ec22fae6
KAROL0820 <karolinee0820@gmail.com>
2022-08-21 17:35:10 +0800

    Update 2022-08-21T09:35:06.204Z

---
+++ assets/js/all.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$(function () {
+  $('.arrow').click(function (e) {
+    e.preventDefault();
+    $('.down').toggleClass('d-none');
+    $('.up').toggleClass('d-none');
+  });
+});
+//# sourceMappingURL=all.js.map
